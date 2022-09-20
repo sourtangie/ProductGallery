9e18b232b073e18354759a57fbf43a2216630aa2
sourtangie <molleetje@gmail.com>
2022-09-20 18:01:45 +0200

    update routing for gh-pages

--- product-gallery/src/util/GetData.ts
+++ product-gallery/src/util/GetData.ts
@@ -1,6 +1,6 @@
 export async function getProductList() {
   const serviceUrl =
-    "http://private-5815fe-recommendationsknip.apiary-mock.com/products";
+    "https://private-5815fe-recommendationsknip.apiary-mock.com/products";
   const response = await fetch(serviceUrl, {
     method: "GET",
   });
